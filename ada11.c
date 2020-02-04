cd92323600fad20f886f4a1a0de5586bfca4db30
rudramuni77 <53811238+rudramuni77@users.noreply.github.com>
2020-02-04 12:33:34 +0530

    Add files via upload

---
+++ ada11.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+int main()
+{
+	int t,n,i,j,ele,a[50];
+	printf("Enter the number of testcases\n");
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+	{
+		printf("\nEnter the number of elements in array and element to be searched\n");
+		scanf("%d %d",&n,&ele);
+		
+		printf("Enter the elements of array\n");
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&a[j]);
+		}
+	for(j=0;j<n;j++)
+		{
+			if(a[j]==ele)
+                        
+		
+			{
+				printf("1 (Element is present)\n");
+				break;
+			}}
+			
+		if(a[j]==n-1)
+                   
+			printf("-1 (Element is not present)\n");
+			
+      }
+	return 0;
+}
+
